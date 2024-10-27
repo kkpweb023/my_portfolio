608808c2246351344b7703247c32b3784ffd7576
kkpweb023 <kpandey023@gmail.com>
2024-10-27 23:12:11 +0530

    start about 4

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './Components/Home/Home';
 import NavBar from './Navbar/NavBar';
 import About from "./Components/About Me/About";
+import Skills from "./Components/Skills/Skills";
 
 
 export const MyContext = createContext();
@@ -25,6 +26,7 @@ function App() {
         <NavBar />
         <Home />
         <About />
+        <Skills />
        
 
 
---
+++ src/Components/Skills/Skills.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Skills.css';
+
+
+const Skills = () =>{
+
+    return(
+
+        <div className="skills_div">
+            Skills page
+        </div>
+    );
+};
+
+export default Skills;
